48a320389864434b43eb8ce300626b6f3eaa374d
cristian-albu <albucristian95p@gmail.com>
2024-04-03 00:10:02 +0300

    planets

--- components/sections/hero/HeroSection.tsx
+++ components/sections/hero/HeroSection.tsx
@@ -3,34 +3,51 @@ import styles from "./HeroSection.module.css";
 import Button from "@/components/layout/button/Button";
 
 const heroData = {
-  preTitle: "Hello",
-  title: "I'm asdsa",
-  subtitle: "Text",
-  cta: "Schedule meeting",
+  preTitle: "Hello there. I'm Cristian",
+  title: "Full stack developer focused on bespoke web applications space",
+  subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  cta: "Let's talk",
 };
 
 const HeroSection = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.bgAnimation}></div>
-      <div className={styles.heroPlanetContainer}>
-        <div className={styles.heroPlanetRing}>
-          <div className={styles.heroPlanetRing}>
-            <div className={styles.heroPlanetRing}>
-              <div className={styles.heroPlanetRing}>
-                <div className={styles.heroPlanetRing} />
+      <div className={styles.heroBg}>
+        <div className={styles.blob} />
+        <div className={styles.blob} />
+        <div className={styles.blob} />
+      </div>
+      <div className={styles.planetSystem}>
+        <div className={styles.ringsContainer}>
+          <div className={styles.ringsSpinner}>
+            <div className={styles.ring}>
+              <div className={styles.ringMoon} />
+              <div className={styles.ring}>
+                <div className={styles.ringMoon} />
+                <div className={styles.ring}>
+                  <div className={styles.ringMoon} />
+                  <div className={styles.ring}>
+                    <div className={styles.ringMoon} />
+                    <div className={styles.ring}>
+                      <div className={styles.ringMoon} />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className={styles.heroPlanet}></div>
+        <div className={styles.planet}></div>
       </div>
-      <div className={styles.heroContainer}>
-        <span>{heroData.preTitle}</span>
-        <h1>{heroData.title}</h1>
-        <p>{heroData.subtitle}</p>
-        <Button>{heroData.cta}</Button>
+
+      <div className={styles.heroWrapper}>
+        <div className={styles.heroContainer}>
+          <span>{heroData.preTitle}</span>
+          <h1>{heroData.title}</h1>
+          <p>{heroData.subtitle}</p>
+          <Button>{heroData.cta}</Button>
+        </div>
       </div>
     </section>
   );
